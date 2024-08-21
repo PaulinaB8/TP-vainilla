685640eaf4815bc08c9defad520242f497a00d53
PaulinaB <paulinabruno343@gmail.com>
2024-08-21 11:53:21 -0300

    Primer Commit

---
+++ index.js
@@ -0,0 +1,54 @@
+
+  let frases = {
+      es: {
+          bienvenido: "BIENVENIDO A SWIFTGRAMMAR",
+          aprende: "Aprendé idiomas con profesores nativos",
+          certifica: "Certifica tu nivel con exámenes internacionales",
+          contacto: "Contactanos",
+          ver: "Ver Más"
+      },
+      en: {
+          bienvenido: "WELCOME TO SWIFTGRAMMAR",
+          aprende: "Learn languages with native teachers",
+          certifica: "Certificate your level with international exams",
+          contacto: "Contact us",
+          ver:"See More"
+      },
+      ita:{
+          bienvenido: "BENVENUTO A SWIFTGRAMMAR",
+          aprende: "Impara lingue con profsessori nativi",
+          certifica: "Certifica il tuo nivello con essami internazionali",
+          contacto: "Contattaci",
+          ver: "Vedere di più"
+      },
+      fr:{
+        bienvenido: "BIENVENUE À SWIFTGRAMMAR",
+        aprende: "Apprenez des langues avec des professeurs natifs",
+        certifica: "Certifiez votre niveau avec des examens internationaux",
+        contacto: "Contactez-nous",
+        ver: "Voir plus"
+    },
+
+  };
+
+  function seleccionarIdioma(){
+      let idiomaSeleccionado = document.getElementById("idioma").value;
+
+      let bienvenido = document.getElementById("bienvenido");
+      bienvenido.innerText = frases[idiomaSeleccionado].bienvenido;
+
+      let aprende = document.getElementById("aprende");
+      aprende.innerText = frases[idiomaSeleccionado].aprende;
+
+      let certifica = document.getElementById("certifica");
+      certifica.innerText = frases[idiomaSeleccionado].certifica;
+    
+      let contacto = document.getElementById("contacto");
+      contacto.innerText = frases[idiomaSeleccionado].contacto;
+
+      let ver = document.getElementById("ver-idiomas");
+      ver.innerText = frases[idiomaSeleccionado].ver;
+    
+  }
+
+
