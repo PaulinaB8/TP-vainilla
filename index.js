6337362467b10c4a8e93d56de5833213e78b274a
PaulinaB <paulinabruno343@gmail.com>
2024-08-24 20:15:01 -0300

    Agregado de más idiomas

--- index.js
+++ index.js
@@ -1,54 +1,64 @@
 
-  let frases = {
-      es: {
-          bienvenido: "BIENVENIDO A SWIFTGRAMMAR",
-          aprende: "Aprendé idiomas con profesores nativos",
-          certifica: "Certifica tu nivel con exámenes internacionales",
-          contacto: "Contactanos",
-          ver: "Ver Más"
-      },
-      en: {
-          bienvenido: "WELCOME TO SWIFTGRAMMAR",
-          aprende: "Learn languages with native teachers",
-          certifica: "Certificate your level with international exams",
-          contacto: "Contact us",
-          ver:"See More"
-      },
-      ita:{
-          bienvenido: "BENVENUTO A SWIFTGRAMMAR",
-          aprende: "Impara lingue con profsessori nativi",
-          certifica: "Certifica il tuo nivello con essami internazionali",
-          contacto: "Contattaci",
-          ver: "Vedere di più"
-      },
-      fr:{
-        bienvenido: "BIENVENUE À SWIFTGRAMMAR",
-        aprende: "Apprenez des langues avec des professeurs natifs",
-        certifica: "Certifiez votre niveau avec des examens internationaux",
-        contacto: "Contactez-nous",
-        ver: "Voir plus"
+let frases = {
+    es: {
+        bienvenido: "BIENVENIDO A SWIFTGRAMMAR",
+        aprende: "Aprendé idiomas con profesores nativos",
+        certifica: "Certifica tu nivel con exámenes internacionales",
+        contacto: "Contactanos",
+        ver: "Ver Más"       
     },
+    en: {
+        bienvenido: "WELCOME TO SWIFTGRAMMAR",
+        aprende: "Learn languages with native teachers",
+        certifica: "Certificate your level with international exams",
+        contacto: "Contact us",
+        ver:"See More"
+    },
+    ita:{
+        bienvenido: "BENVENUTO A SWIFTGRAMMAR",
+        aprende: "Impara lingue con profsessori nativi",
+        certifica: "Certifica il tuo nivello con essami internazionali",
+        contacto: "Contattaci",
+        ver: "Vedere di più"
+    },
+    fr:{
+      bienvenido: "BIENVENUE À SWIFTGRAMMAR",
+      aprende: "Apprenez des langues avec des professeurs natifs",
+      certifica: "Certifiez votre niveau avec des examens internationaux",
+      contacto: "Contactez-nous",
+      ver: "Voir plus"
+    },
+    al:{
+        bienvenido: "WillkommenWILLKOMMEN BEI SWIFTGRAMMAR",
+        aprende: "Lernen Sie Sprachen mit muttersprachlichen Lehrern",
+        certifica: "Zertifizieren Sie Ihr Niveau mit internationalen Prüfungen",
+        contacto: "Kontaktieren Sie uns",
+        ver: "Mehr sehen",
+    },
+    port:{
+        bienvenido: "BEM-VINDO A SWIFTGRAMMAR",
+        aprende: "Aprenda idiomas com professores nativos",
+        certifica: "Certifique seu nível com exames internacionais",
+        contacto: "Contate-nos",
+        ver: "Ver mais"
+    }
+    };
 
-  };
+function seleccionarIdioma(){
+    let idiomaSeleccionado = document.getElementById("idioma").value;
 
-  function seleccionarIdioma(){
-      let idiomaSeleccionado = document.getElementById("idioma").value;
+    let bienvenido = document.getElementById("bienvenido");
+    bienvenido.innerText = frases[idiomaSeleccionado].bienvenido;
 
-      let bienvenido = document.getElementById("bienvenido");
-      bienvenido.innerText = frases[idiomaSeleccionado].bienvenido;
+    let aprende = document.getElementById("aprende");
+    aprende.innerText = frases[idiomaSeleccionado].aprende;
 
-      let aprende = document.getElementById("aprende");
-      aprende.innerText = frases[idiomaSeleccionado].aprende;
+    let certifica = document.getElementById("certifica");
+    certifica.innerText = frases[idiomaSeleccionado].certifica;
 
-      let certifica = document.getElementById("certifica");
-      certifica.innerText = frases[idiomaSeleccionado].certifica;
-    
-      let contacto = document.getElementById("contacto");
-      contacto.innerText = frases[idiomaSeleccionado].contacto;
+    let contacto = document.getElementById("contacto");
+    contacto.innerText = frases[idiomaSeleccionado].contacto;
 
-      let ver = document.getElementById("ver-idiomas");
-      ver.innerText = frases[idiomaSeleccionado].ver;
-    
-  }
-
-
+    let ver = document.getElementById("ver-idiomas");
+    ver.innerText = frases[idiomaSeleccionado].ver;
+}
--- veridiomas.js
+++ veridiomas.js
@@ -6,7 +6,8 @@ let frases={
         italiano: "Clases de italiano",
         frances: "Clases de francés",
         aleman: "Clases de alemán",
-        portugues: "Clases de portugués"
+        portugues: "Clases de portugués",
+        contacto: "Contactanos"
     },
     en:{
         aprende : "Learn languages ​​in just one click",
@@ -15,7 +16,8 @@ let frases={
         italiano: "Italian classes",
         frances: "French",
         aleman: "German classes",
-        portugues: "Potuguese classes"
+        portugues: "Potuguese classes",
+        contacto: "Contact us"
     },
     ita:{
         aprende : "Impara le lingue in un solo clic",
@@ -24,7 +26,8 @@ let frases={
         italiano: "Lezioni di italiano",
         frances: "Lezioni di francese",
         aleman: "Lezioni di tedesco",
-        portugues: "Lezioni di portoghese"
+        portugues: "Lezioni di portoghese",
+        contacto: "Contattaci"
     },
     fr:{
         aprende : "Apprenez des langues en un seul clic",
@@ -33,9 +36,29 @@ let frases={
         italiano: "Cours d'italien",
         frances: "Cours de français",
         aleman: "Cours d'allemand",
-        portugues: "Cours de portugais"
+        portugues: "Cours de portugais",
+        contacto: "Contactez-nous"
     },
-
+    al:{
+        aprende : "Lernen Sie Sprachen mit nur einem Klick",
+        español: "Spanischunterricht",
+        ingles: "Englischunterricht",
+        italiano: "Italienischunterricht",
+        frances: "Französischunterricht",
+        aleman: "Deutschunterricht",
+        portugues: "Portugiesischunterricht",
+        contacto: "Kontaktieren Sie uns"
+    },
+    port:{
+        aprende : "Aprenda idiomas com apenas um clique",
+        español: "Aulas de espanhol",
+        ingles: "Aulas de inglês",
+        italiano: "Aulas de italiano",
+        frances: "Aulas de francês",
+        aleman: "Aulas de alemão",
+        portugues: "Aulas de português",
+        contacto: "Contate-nos"
+    }
 }
 
 function seleccionarIdioma(){
@@ -61,4 +84,7 @@ function seleccionarIdioma(){
 
     let portugues = document.getElementById("portugues");
     portugues.innerText = frases[idiomaSeleccionado].portugues;
+
+    let contacto = document.getElementById("contacto");
+    contacto.innerText = frases[idiomaSeleccionado].contacto;
 }
